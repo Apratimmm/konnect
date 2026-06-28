df457516f8fc008bc913a0063bcd4001ba798586
Apratimmm <Aapratimboy@gmail.com>
2026-06-28 21:44:59 +0545

    deployment ready???

--- main/settings.py
+++ main/settings.py
@@ -2,6 +2,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 import dj_database_url
+import django.dispatch
+
+original_signal_init = django.dispatch.Signal.__init__
+def new_signal_init(self, *args, **kwargs):
+    kwargs.pop('providing_args', None)
+    return original_signal_init(self, *args, **kwargs)
+django.dispatch.Signal.__init__ = new_signal_init
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
