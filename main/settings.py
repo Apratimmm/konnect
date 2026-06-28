1cd097569dc8ead4725054c8afee9cd7562f9097
Apratimmm <Aapratimboy@gmail.com>
2026-06-28 21:22:40 +0545

    deployment ready

--- main/settings.py
+++ main/settings.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
-
-load_dotenv()
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / '.env')
+
 MEDIA_ROOT= BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
@@ -12,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-dev-key-change-me
 
 DEBUG = os.getenv('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'daphne',
@@ -30,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,8 +46,9 @@ EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv('user')
-EMAIL_HOST_PASSWORD = os.getenv('password')
+EMAIL_HOST_USER = 'b0377b001@smtp-brevo.com'
+EMAIL_HOST_PASSWORD = 'xsmtpsib-19269c4601e6bce2eb27c6d88e3e09d2aaa2e0a3bc30f750f8dd5e5284020661-04VmAP66M0pF0TNd'
+
 DEFAULT_FROM_EMAIL = 'Konnect <Aapratimboy@gmail.com>'
 SERVER_EMAIL = 'Aapratimboy@gmail.com'
 
@@ -73,17 +76,18 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [('localhost', 6379)],
-        }
-    }
+            'hosts': [os.getenv('REDIS_URL')],
+        },
+    },
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(
+            default=os.getenv('DB_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
 
 AUTH_USER_MODEL = 'main.User'
 
@@ -114,3 +118,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
